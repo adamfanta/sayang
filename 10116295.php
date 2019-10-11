41103af1706303398b5584e78c3b3c3ed7dfe9d9
adamfanta <50398151+adamfanta@users.noreply.github.com>
2019-10-11 16:56:57 +0700

    Add files via upload

---
+++ 10116295.php
@@ -0,0 +1,14 @@
+<html>
+<?php 
+<table border="1" cellpadding="10">
+        <tr>
+            <td bgcolor="yellow">Nim</td>
+           <td bgcolor="yellow">Nama</td>
+        </tr>
+        <tr bgcolor="#00ff80">
+            <td>10116295</td>
+            <td>Adam Aris Rizaldy</td>
+        </tr>
+    </table>
+?>
+</html>
